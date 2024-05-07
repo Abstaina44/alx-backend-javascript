ef6ef2dad227cd2c025d6836492e107ee6e05868
Abstaina44 <padcliff88@gmail.com>
2024-05-07 11:11:44 +0000

    bundle.js

---
+++ 0x04-TypeScript/task_5/dist/bundle.js
@@ -0,0 +1,12 @@
+import { Module1 } from './module1';
+import { Module2 } from './module2';
+
+// Import your modules here
+
+// Your code goes here
+const module1 = new Module1();
+const module2 = new Module2();
+
+// Call functions or perform operations using the imported modules
+module1.doSomething();
+module2.doSomethingElse();
